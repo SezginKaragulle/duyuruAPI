4469a8d4982a69bfcc992c63ce597e2620c606f1
Sezgin Karagülle <52202426+SezginKaragulle@users.noreply.github.com>
2022-03-15 14:31:16 +0300

    Update main.go

--- main.go
+++ main.go
@@ -6,11 +6,24 @@ import (
 	"net/http"
 )
 
+type HttpAddresses struct {
+	Adress string
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World %s!", r.URL.Path[1:])
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/getUsers/", getUsers)
+	http.HandleFunc("/createUsers/", createUser)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Created User", r.URL.Path[1:])
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "getUsers", r.URL.Path[1:])
 }
