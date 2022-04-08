551c43f90c200c9eb6950e50aba4fa9b7ccec7e9
Sezgin Karagülle <52202426+SezginKaragulle@users.noreply.github.com>
2022-04-08 12:05:28 +0300

    arranging for groups
    it is arranged for groups.

--- modals/groups.go
+++ modals/groups.go
@@ -19,22 +19,9 @@ type Groups struct {
 }
 
 type GroupMembers struct {
-	ID      int64 `json:"_id,omitempty" bson:"_id,omitempty"`
-	UserID  int64 `json:"userID,omitempty" bson:"userID,omitempty"`
-	GroupID int64 `json:"groupID,omitempty" bson:"groupID,omitempty"`
-}
-
-type Groups2 struct {
-	ID           int64         `json:"_id,omitempty" bson:"_id,omitempty"`
-	CreaterID    int64         `json:"createrID,omitempty" bson:"createrID,omitempty"`
-	Name         string        `json:"name" bson:"name,omitempty"`
-	GroupMembers *GroupMembers2 `json:"groupMembers" bson:"groupMembers,omitempty"`
-}
-
-type GroupMembers2 struct {
-	ID      int64   `json:"_id,omitempty" bson:"_id,omitempty"`
-	UserID  []int   `json:"userID,omitempty" bson:"userID,omitempty"`
-	GroupID int64   `json:"groupID,omitempty" bson:"groupID,omitempty"`
+	ID      int64    `json:"_id,omitempty" bson:"_id,omitempty"`
+	UserID  []string `json:"userID,omitempty" bson:"userID,omitempty"`
+	GroupID int64    `json:"groupID,omitempty" bson:"groupID,omitempty"`
 }
 
 func CreateGroup(w http.ResponseWriter, r *http.Request) {
@@ -43,17 +30,20 @@ func CreateGroup(w http.ResponseWriter, r *http.Request) {
 	var collection = ConnectDB("groups")
 	var params = mux.Vars(r)
 
+	//For Groups
 	myID, _ := strconv.Atoi(params["id"])
 	myCreaterID, _ := strconv.Atoi(params["createrID"])
 	myName, _ := params["name"]
+    
 
+	//For GroupMembers
 	myGroupMemberID, _ := strconv.Atoi(params["memberID"])
-	myUserID, _ := strconv.Atoi(params["userID"])
+	myUserIDSlices := []string{params["userID"]}
 	myGroupID, _ := strconv.Atoi(params["groupID"])
 
 	newMyGroupMembers := GroupMembers{
 		ID:      int64(myGroupMemberID),
-		UserID:  int64(myUserID),
+		UserID:  myUserIDSlices,
 		GroupID: int64(myGroupID),
 	}
 
@@ -120,79 +110,4 @@ func DeleteGroups(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(deleteResult)
-}
-
-//Following functions for test
-
-func CreateGroup2(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	var collection = ConnectDB("groups")
-	var params = mux.Vars(r)
-	//mySlc2 := []int{1,2,3,4,5}
-	myID, _ := strconv.Atoi(params["id"])
-	myCreaterID, _ := strconv.Atoi(params["createrID"])
-	myName, _ := params["name"]
-	
-
-	myGroupMemberID, _ := strconv.Atoi(params["memberID"])
-	//myUserID, _ := strconv.Atoi(params["userID"])
-	myUserID, _ := strconv.Atoi(params["userID"])
-	myUserIDSlices:= []int{}
-	myUserIDSlices = append(myUserIDSlices, myUserID)
-	
-	myGroupID, _ := strconv.Atoi(params["groupID"])
-
-	newMyGroupMembers := GroupMembers2{
-		ID:      int64(myGroupMemberID),
-		UserID:  myUserIDSlices,
-		GroupID: int64(myGroupID),
-	}
-
-	newGroups := Groups2{
-		ID:           int64(myID),
-		CreaterID:    int64(myCreaterID),
-		Name:         myName,
-		GroupMembers: &newMyGroupMembers,
-	}
-	_ = json.NewDecoder(r.Body).Decode(&newGroups)
-
-	result, err := collection.InsertOne(context.TODO(), newGroups)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	json.NewEncoder(w).Encode(result)
-}
-
-func GetGroups2(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var collection = ConnectDB("groups")
-	var myGroups []Groups2
-
-	cur, err := collection.Find(context.TODO(), bson.M{})
-
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	defer cur.Close(context.TODO())
-
-	for cur.Next(context.TODO()) {
-
-		var myGroup Groups2
-		err := cur.Decode(&myGroup)
-		if err != nil {
-			log.Fatal(err)
-		}
-		myGroups = append(myGroups, myGroup)
-	}
-
-	if err := cur.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	json.NewEncoder(w).Encode(myGroups)
 }
